ee3b74a308f52e0f13cc681df2b87ec4646b07b9
Devin Lin <dlin01@stuy.edu>
2019-10-27 15:17:04 -0400

    merge

--- db_builder.py
+++ db_builder.py
@@ -21,10 +21,11 @@ create_table_users = "CREATE TABLE users(user_id INTEGER PRIMARY KEY AUTOINCREME
 c.execute(create_table_users)
 c.execute(insert_one)
 
-# insert_story = "INSERT INTO stories(user_id, title, full_text, edit) VALUES( \"admin\", \"hello\", \"The farmer went to the barn and said hello to his animals\", \"The farmer went to the barn and said hello to his animals.\");"
-# create_table_stories = "CREATE TABLE stories(story_id INTEGER PRIMARY KEY, user_id INTEGER DEFAULT NULL, title TEXT, full_text TEXT, edit TEXT);"
+insert_story = "INSERT INTO stories(user_id, title, full_text, edit) VALUES( \"admin\", \"hello\", \"The farmer went to the barn and said hello to his animals\", \"The farmer went to the barn and said hello to his animals.\");"
+create_table_stories = "CREATE TABLE stories(story_id INTEGER PRIMARY KEY, user_id INTEGER DEFAULT NULL, title TEXT, full_text TEXT, edit TEXT);"
 c.execute(create_table_stories)
 c.execute(insert_story)
+
 #==========================================================
 
 db.commit()  # save changes
