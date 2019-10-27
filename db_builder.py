b5e2f4eb1170a6c2a7d5b7e9f405e96fc552efd6
Devin Lin <dlin01@stuy.edu>
2019-10-27 16:51:34 -0400

    reverted

--- db_builder.py
+++ db_builder.py
@@ -17,13 +17,19 @@ c = db.cursor()  # facilitate db ops
 
 # test SQL stmt in sqlite3 shell, save as string
 create_table_users = "CREATE TABLE users(user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, date_created DATETIME DEFAULT current_timestamp);"
+insert_user = "INSERT INTO users(user_id, username, password) VALUES( 1, \"admin\", \"peterstuy\");"
 c.execute(create_table_users)
+c.execute(insert_user)
 
 create_table_stories = "CREATE TABLE stories(story_id INTEGER PRIMARY KEY AUTOINCREMENT, author_id INTEGER DEFAULT NULL, title TEXT, body TEXT);"
+insert_story = "INSERT INTO stories(story_id, author_id, title, body) VALUES( 1, 1, \"hello\", \"The farmer went to the barn and said hello to his animals\");"
 c.execute(create_table_stories)
+c.execute(insert_story)
 
 create_table_edits = "CREATE TABLE edits(story_id INTEGER, user_id INTEGER, edit TEXT, timestamp DATETIME DEFAULT current_timestamp);"
+insert_edit = "INSERT INTO edits(story_id, user_id, edit) VALUES( 1, 1, \"The farmer went to the barn and said hello to his animals\");"
 c.execute(create_table_edits)
+c.execute(insert_edit)
 
 #==========================================================
 
