ce0a97efe001823310dd19d6cf4a88d38fd7b643
ray. lee <rlee00@stuy.edu>
2020-02-04 21:53:53 -0500

    db path

--- hotcocoa/utl/db_functions.py
+++ hotcocoa/utl/db_functions.py
@@ -10,7 +10,7 @@ import sqlite3  # enable control of an sqlite database
 import os
 
 DIR = os.path.dirname(__file__) or '.'
-DIR += '/'
+DIR += '/../'
 DB_FILE = DIR + "wiki.db"
 db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
 c = db.cursor()  # facilitate db ops
